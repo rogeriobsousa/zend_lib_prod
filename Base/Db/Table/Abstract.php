bb5f33d029785e78503dfd7a04b9f021fef1735a
Rogerio Baptista de Sousa <rogeriosousa@unb.br>
2018-09-18 11:40:28 -0300

    auditoria

--- Base/Db/Table/Abstract.php
+++ Base/Db/Table/Abstract.php
@@ -503,7 +503,7 @@ class Base_Db_Table_Abstract extends Zend_Db_Table_Abstract
 				}else if(isset($this->_metadata[$key]['DATA_TYPE']) and in_array($this->_metadata[$key]['DATA_TYPE'],$arrayNum) and is_numeric($value)){
 					$arraySelect[] = $this->_name.'.'.$key." = ".$value;
 				}else if(isset($this->_metadata[$key]['DATA_TYPE']) and in_array($this->_metadata[$key]['DATA_TYPE'],$arrayDate)){
-					$arraySelect[] = $this->_name.'.'.$key."::text like '%".$value."%'";
+					$arraySelect[] = $this->_name.'.'.$key."::text ilike '%".$value."%'";
 				}else if (isset($this->_metadata[$key])){
 					
 				}
@@ -512,5 +512,18 @@ class Base_Db_Table_Abstract extends Zend_Db_Table_Abstract
 			return $arraySelect;
 		}
 		return null;	
+	}
+	
+	public function sanitizeString($str) {
+	    $str = preg_replace('/[áàãâä]/ui', 'a', $str);
+	    $str = preg_replace('/[éèêë]/ui', 'e', $str);
+	    $str = preg_replace('/[íìîï]/ui', 'i', $str);
+	    $str = preg_replace('/[óòõôö]/ui', 'o', $str);
+	    $str = preg_replace('/[úùûü]/ui', 'u', $str);
+	    $str = preg_replace('/[ç]/ui', 'c', $str);
+	    // $str = preg_replace('/[,(),;:|!"#$%&/=?~^><ªº-]/', '_', $str);
+	    $str = preg_replace('/[^a-z0-9]/i', '_', $str);
+	    $str = preg_replace('/_+/', '_', $str); // ideia do Bacco :)
+	    return $str;
 	}
 }
